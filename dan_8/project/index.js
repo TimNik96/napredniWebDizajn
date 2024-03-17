feb400fde1075d35d43622d1733661e0084e67f9
nikolatimotic <mail>
2024-03-17 20:00:03 +0100

    burger menu functionality and finish couple more sections

--- dan_8/project/index.js
+++ dan_8/project/index.js
@@ -1,1 +1,13 @@
+const burgerMenu = document.querySelector("header svg")
+const sideMenu = document.querySelector(".side-menu")
+const closeSideMenu = document.querySelector(".close svg")
+
+burgerMenu.addEventListener("click", () => {
+    sideMenu.style.left = "0"
+})
+
+closeSideMenu.addEventListener("click", () => {
+    sideMenu.style.left = "-300px"
+})
+
 AOS.init();
